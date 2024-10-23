67a6f43c7d9105ad27c7ad9fe10e027d3d9c9f62
= <108800565+skytechrick01@users.noreply.github.com>
2024-10-23 21:31:21 +0530

    QR done completed

--- Controllers_Page/Admin_QR.js
+++ Controllers_Page/Admin_QR.js
@@ -5,7 +5,6 @@ const QRCode = require('qrcode');
 const ADMIN_PASS = process.env.ADMIN_PASS;
 const { Verify_Token , Generate_Token } = require('../utils/JWT.js');
 const { Admin_User , Assistants , Qr_Codes } = require('../Models.js');
-const QR_Codes = require('../Routes/Admin_QR_Codes.js');
 
 function Product_ID() {
     const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -17,6 +16,15 @@ function Product_ID() {
     };
     return result;
 };
+
+
+// Protocol 
+let Protocol = "http";
+if (process.env.NODE_ENV === 'production') {
+    Protocol = 'https';
+};
+
+const Project_URL = `${Protocol}://${process.env.PROJECT_DOMAIN}`;
 
 
 const QR_DATA = async (TotalRequired)=>{
@@ -100,7 +108,7 @@ const QR_deleted = async ( req , res , next )=>{
 const QR_final = async ( req , res , next )=>{
     try{
         const Data = await Qr_Codes.findOne({_id:"GSB1234"});
-        await QR.updateOne({_id:"GSB1234"},{$set:{
+        await Qr_Codes.updateOne({_id:"GSB1234"},{$set:{
             Created_QR_Codes: [...Data.Created_QR_Codes, ...Data.Temporary_QR_Codes],
             Temporary_QR_Codes: []
         }}).then(()=>{
@@ -135,7 +143,7 @@ const QR_Search = async ( req , res , next )=>{
 
 const QR_Delete_QR = async ( req , res , next )=>{
     try{
-        const Data = await QR.findOne({_id:"GSB1234"});
+        const Data = await Qr_Codes.findOne({_id:"GSB1234"});
         let QR1 = req.body.QR_CODE;
         if(QR1){
             QR1 = QR1.toUpperCase();
@@ -144,6 +152,7 @@ const QR_Delete_QR = async ( req , res , next )=>{
             for(let i = 0; i < Data.Created_QR_Codes.length; i++){
                 if(Data.Created_QR_Codes[i] == QR1){
                     found = true;
+                    continue;
                 };
                 D.push(Data.Created_QR_Codes[i]);
             };
@@ -165,6 +174,88 @@ const QR_Delete_QR = async ( req , res , next )=>{
     };
 };
 
+const QR_PAGE = async ( req , res , next ) => {
+    try {
+        const Options_For_QR = {
+            version: 3,
+            errorCorrectionLevel: 'L', 
+            margin: 1,
+            width: 200,
+            color:{
+                dark: '#000000',
+                light: '#ffffff',
+            },
+        };
+
+
+
+
+
+
+
+
+
+
+
+        let ST;
+        async function AA(Create) {
+            let D = [];
+            for (let index = 0; index < Create.length; index++) {
+                const element = Create[index];
+                const dataUrl = await new Promise((resolve, reject) => {
+                    QRCode.toDataURL(`${Project_URL}/qr/${element}`, Options_For_QR, (err, url) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(url);
+                        }
+                    });
+                });
+                D.push({ ID: element, Image: String(dataUrl) });
+            };
+            return D;
+        }
+
+        
+        const Data = await Qr_Codes.findOne({_id:"GSB1234"});
+        if(Data.Temporary_QR_Codes.length != 0){
+            
+            ST = await AA(Data.Temporary_QR_Codes);
+            // ST = JSON.stringify(ST);
+            const Final_JSON = ST;
+
+            
+            let DDDD = "";
+            let Images = Final_JSON;
+            // console.log(Images);
+            for (let index = 0; index < Images.length; index++) {
+                const element = Images[index];
+                // console.log(element.ID);
+                DDDD += `
+                    <div class="QR">
+                        <div class="TOP">GETSKYBUY.IN   <span class="Circle"></span></div>
+                        <img src="${element.Image}" alt="Image">
+                        <div class="CODE">${element.ID}</div>
+                        <div class="Company">GET SKY BUY</div>
+                    </div>
+                `;
+            };
+            
+            
+            return res.status(200).render("QR_Page", {UniqueCodes: DDDD});
+
+        }else{
+            return res.status(400).json({Message: "Please generate the QR Codes first"});
+        };
+        
+        
+        
+    } catch (error) {
+        next(error);
+    };
+};
+
+
 module.exports = {
     QR_HOMEPAGE,
     QR_code_Generate,
@@ -172,4 +263,5 @@ module.exports = {
     QR_final,
     QR_Search,
     QR_Delete_QR,
+    QR_PAGE,
 }
--- Routes/Admin_QR_Codes.js
+++ Routes/Admin_QR_Codes.js
@@ -12,7 +12,7 @@ QR_Codes.use(cookieParser(process.env.COOKIE_SECRET));
 const Verify_User_Page  = require('../utils/Verify_User_Page.js');
 const Verify_User_API  = require('../utils/Verify_User_API.js');
 
-const { QR_HOMEPAGE , QR_code_Generate , QR_deleted , QR_final , QR_Search , QR_Delete_QR } = require('../Controllers_Page/Admin_QR.js');
+const { QR_HOMEPAGE , QR_code_Generate , QR_deleted , QR_final , QR_Search , QR_Delete_QR , QR_PAGE } = require('../Controllers_Page/Admin_QR.js');
 
 QR_Codes.get("/", Verify_User_Page , QR_HOMEPAGE );
 QR_Codes.post("/generate_codes", Verify_User_API , QR_code_Generate );
@@ -20,3 +20,4 @@ QR_Codes.delete("/temp", Verify_User_API , QR_deleted );
 QR_Codes.put("/final", Verify_User_API , QR_final );
 QR_Codes.post("/search", Verify_User_API , QR_Search );
 QR_Codes.delete("/qr-delete", Verify_User_API , QR_Delete_QR );
+QR_Codes.get("/qr-page", Verify_User_Page , QR_PAGE );
