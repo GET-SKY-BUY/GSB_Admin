aa9c1314ab0ef536b12169c842bcdd62f85ea4b8
= <ricksarkar2005@gmail.com>
2024-10-29 21:59:43 +0530

    Continuation with product Assistant

--- Controllers/Product_Image_Processing.js
+++ Controllers/Product_Image_Processing.js
@@ -6,7 +6,6 @@ const sharp = require('sharp');
 async function processImage(fileName) {
     try {
         const inputPath = path.join(__dirname, '../Uploaded_Product_Images', fileName);
-        // const bgRemovedImagePath = path.join(__dirname, '../On_Processing_Images', `${Date.now()}_Bg_removed.png`);
         const outputFilePath = path.join(__dirname, '../Converted_Images', `${path.parse(fileName).name}_${Date.now()}_Save.webp`);
         
         let image = sharp(inputPath);
@@ -21,15 +20,12 @@ async function processImage(fileName) {
         image = image.flatten({ background: { r: 255, g: 255, b: 255, alpha: 1 } });
         await image.toFormat('webp', { quality: 95 }).toFile(outputFilePath);
 
-        return outputFilePath;
+        return path.basename(outputFilePath);
     } catch (error) {
         return null;
     };
 };
 
-// Usage example
-// processImage('Saving ATM2.jpg');
-(async () => {
-    let a = await processImage('My_Image-01-removebg-preview.png');
-    console.log('Done: ' , a);
-})();
+// let a = processImage('My_Image-01-removebg-preview.png');
+
+module.exports = processImage;
